8aaf22419b1e9d709fa54552e718178574b0c22d
mek1999 <47376832+mek1999@users.noreply.github.com>
2020-02-26 21:55:52 +0530

    Create strnumeric.py

---
+++ strnumeric.py
@@ -0,0 +1,5 @@
+s=input()
+for i in 10 :
+  if s[i]=='0' or s[i]=='1' or s[i]=='2' or s[i]=='3' or s[i]=='4' or s[i]=='5' or s[i]=='6' or s[i]=='7' or  s[i]=='8' or s[i]=='9' :
+    print("yes")
+print("no")
